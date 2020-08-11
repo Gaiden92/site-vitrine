cce6ffdabb468eb73e8896162c560ef1bd9883fb
Sami <samy_fouchal@hotmail.fr>
2020-08-11 12:18:55 +0200

    animation menu scroll

---
+++ parametres.php
@@ -0,0 +1,11 @@
+<?php 
+/**
+ * Fichier contenant toutes les constantes liées à la configuration 
+ * du site
+ */
+
+// Paramètres de connexion MySQL
+const DB_HOST = 'sql25';
+const DB_NAME = 'svd36351';
+const DB_USER = 'root';
+const DB_PASS = 'm0Qg9EXKV4dY';
